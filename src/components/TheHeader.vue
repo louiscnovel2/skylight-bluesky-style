a9ca82a3e9a47e9fc3db9bc23445b284c4325e28
louiscnovel2 <127027744+louiscnovel2@users.noreply.github.com>
2023-07-29 02:21:59 +0900

    タイトルを変更
    h1のタイトルをSkylightからSkylight Bluesky Styleへと変更しました。

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -36,7 +36,7 @@ const tabs = [
 
 <template>
   <div class="columns col-oneline p-2">
-    <h1 class="col-10" style="font-family: 'Segoe UI Mono'">Skylight</h1>
+    <h1 class="col-10" style="font-family: 'Segoe UI Mono'">Skylight Bluesky Style</h1>
     <button
       v-if="route.name !== 'login'"
       class="btn btn-link col-ml-auto"
